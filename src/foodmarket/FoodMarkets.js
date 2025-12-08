296eabd9b616b5572c9bdebedde0861c58e6a37f
heolyun <hl620@naver.com>
2025-12-08 15:54:49 +0900

    커밋

--- src/foodmarket/FoodMarkets.js
+++ src/foodmarket/FoodMarkets.js
@@ -15,21 +15,26 @@ import food3 from './img/food3.jpg';
 import foodsData from './data/foodsData';
 import { useState } from "react";
 import FoodCard from "./components/FoodCard";
+import Home from "./pages/Home";
+import { Routes,Route,Link, useNavigate } from "react-router";
+import CustomerService from "./pages/CustomerService";
 
 export default function FoodMarket() {
 
     let [foods, setFoods] = useState(foodsData);
+    let navigate = useNavigate();
 
     return (
         <div>
 
             <Navbar bg="light" data-bs-theme="light">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand onClick={()=>{navigate("/")}}>FoodMarket</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+                        {/* <Nav.Link href="#features"><Link to="/detail">FoodDetail</Link></Nav.Link> */}
+                        <Nav.Link onClick={()=>{navigate('/detail')}}>FoodDetail</Nav.Link>
+                        <Nav.Link onClick={()=>{navigate('/info')}}>Info</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
@@ -40,18 +45,18 @@ react 컴포넌트 js에서 이미 사용시, import 후 사용
 <img src="https://...외부 url/"/>
 <img src={importimage}/> @@ import @@
 */}
-            <div className="main-bg" style={{ backgroundImage: "url(" + banner_bg + ")" }}></div>
+            {/* <div className="main-bg" style={{ backgroundImage: "url(" + banner_bg + ")" }}></div> */}
 
-            <Container>
+            {/* <Container>
                 <Row>
 
-                    {
+                    {/* {
                         foods.map((food, index)=>{
                             return (<Col md={4} sm={2}>
                             <FoodCard food={food} foods= {foods} index={index}/>
                             </Col>)
                         })
-                    }
+                    } */}
                     {/* <Col md={4} sm={2}>
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={food1} />
@@ -93,8 +98,15 @@ react 컴포넌트 js에서 이미 사용시, import 후 사용
                             </Card.Body>
                         </Card>
                     </Col> */}
-                </Row>
-            </Container>
+                {/* </Row> */}
+            {/* </Container> */ }
+
+            <Routes>
+                <Route path = "/" element={<Home foods={foods}/>}></Route>
+                <Route path = "/detail" element={<div><h1>detail page</h1></div> }></Route>
+                <Route path = "/infoo" element={<div><h1>info</h1></div>}></Route>
+                <Route path = "/help" element={<CustomerService></CustomerService>}></Route>
+            </Routes>
 
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TempApp from './TempApp';
+import { BrowserRouter } from "react-router";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    
     {/* <TempApp/> */}
   </React.StrictMode>
 );
